bb009f348681d1a78a51c04a11025e633f7c0c5d
Chanh Nghia <chanhnghia2409@gmail.com>
2020-06-03 23:03:00 +0700

    first commit

---
+++ Routes/user.routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.model');
+const RequireLogin = require('../middleware/RequireLogin')
+require('dotenv').config()
+
+
+
+router.get('/user/me', RequireLogin, (req, res) => {
+    res.send(req.user)
+})
+
+
+//get user by id
+router.get('/user/:id', RequireLogin, (req, res) => {
+    User.findOne({ _id: req.params.id })
+        .select("-password")
+        .then(user => {
+            res.json(user);
+        }).catch(err => {
+            return res.status(404).json({ error: "Người dùng không tồn tại" })
+        })
+})
+
+
+
+router.put('/user/me/update', RequireLogin, (req, res) => {
+    User.findByIdAndUpdate(req.user._id, { $set: { name: req.body.name, email: req.body.email, avatar: req.body.avatar } }, { new: true },
+        (err, result) => {
+            if (err) {
+                return res.status(422).json({ error: "Không thể Edit" })
+            }
+            res.json(result)
+        })
+})
+
+
+
+
+
+
+module.exports = router;
